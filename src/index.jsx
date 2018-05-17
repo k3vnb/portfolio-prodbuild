8b8bb18eb69caa6aa732d7bd05f463befb70ef61
Kevin Boyle <papershack@gmail.com>
2018-05-17 15:48:09 -0700

    add npm, install React & React-DOM & Webpack, create .gitignore

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <h3>from a computer interface</h3>
+    <p>Direct to you</p>
+  </div>
+  document.getElementById('react-app-root')
+);
+
+
+
+
+
+
+// function liveTime(){
+//   var timeSimplified = new Date().toLocaleTimeString().replace(/:\d+ /, ' ');
+//   var greeting = React.createElement('h1', {}, 'Hello, World!');
+//   var clock = React.createElement('h2', {}, `It is ${timeSimplified}`);
+//   var app = React.createElement('div', {}, greeting, clock);
+//
+//   ReactDOM.render(
+//     app,
+//     document.getElementById('react-app-root')
+//   );
+// }
+// setInterval(liveTime, 1000);
