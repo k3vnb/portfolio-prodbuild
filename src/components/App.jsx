29c1942f2362c8982a76ab71b791e9776bf9570a
Kevin Boyle <papershack@gmail.com>
2018-05-17 16:32:31 -0700

    set first major component child of App - Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
   return (
     <div>
+      <Header />
       <h1>Hello World</h1>
       <h3>from a new computer interface</h3>
       <p>Direct to you</p>
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(){
+  return (
+    <h1>About Kv</h1>
+  );
+}
+
+export default Header;
