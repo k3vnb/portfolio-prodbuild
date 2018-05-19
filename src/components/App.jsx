8b13f4898965564641ba9f30c1c9555d1ffec78d
Kevin Boyle <papershack@gmail.com>
2018-05-18 19:42:57 -0700

    set up router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header';
 import PictureList from './PictureList';
 import Footer from './Footer';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
     <div>
       <Header />
-      <PictureList />
+      <Switch>
+        <Route exact path = "/" component={PictureList} />
+      </Switch>
       <Footer />
     </div>
   );
