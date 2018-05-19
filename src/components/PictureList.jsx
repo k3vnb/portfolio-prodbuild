c6c2a5a49141a32dcbcde44635c1a78c6149e20c
Kevin Boyle <papershack@gmail.com>
2018-05-18 17:10:04 -0700

    add basic looping function

--- src/components/PictureList.jsx
+++ src/components/PictureList.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Picture from './Picture';
 
+var masterPictureList = [
+  {
+    imageSource: '1A',
+    imageTitle: 'Pic1'
+  },
+  {
+    imageSource: '1B',
+    imageTitle: 'Pic2'
+  },
+  {
+    imageSource: '1C',
+    imageTitle: 'Pic3'
+  }
+]
+
 function PictureList(){
   return (
-    <Picture 
-      imageSource = "1A"
-      imageTitle = "Pic1"
-    />
+    <div>
+      <hr/>
+      {masterPictureList.map((picture, index) =>
+      <Picture imageSource = {picture.imageSource}
+        imageTitle = {picture.imageTitle} />
+      )}
+    </div>
   );
 }
 
